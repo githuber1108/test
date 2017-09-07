0c6b28abf8d52a7ff13f004619ba4c8048b15d48
githuber1108 <30859335+githuber1108@users.noreply.github.com>
2017-09-07 14:56:44 +0800

    Delete mysql_monitor.sh

--- mysql_monitor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mysql -uroot -plava@123  <<EOF
-use lavareport
-insert into  tb_lava_monitor_object (monitorSubjectId,stat,status) values ($1,'`date -d'1 hour' '+%Y-%m-%d %H:%M:%S'`',0)
-EOF
-
